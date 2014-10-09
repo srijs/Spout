9b170b0fd30ded4bc2fd89edc3ac6aa9d8fe4bcb
Sam Reis <sreis@atlassian.com>
2014-10-10 01:57:43 +1100

    general producer

--- Stream.hs
+++ Stream.hs
@@ -2,46 +2,33 @@ module Stream where
 
 import Control.Monad
 
-data Stream s e v = Pending s (s -> (Stream s e v))
-                  | Data v (Stream s e v)
-                  | End
-                  | Error e
+data Stream m e r v = Pending (m (Stream m e r v))
+                    | Data v (Stream m e r v)
+                    | End r
+                    | Error e
 
-type Chunk e v = Either (Maybe e) v
+type Chunk e r v = Either (Either e r) v
 
-read :: (Stream s e v) -> (Chunk e v, Stream s e v)
-read (End)         = (Left Nothing, End)
-read (Error e)     = (Left (Just e), Error e)
-read (Data v m)    = (Right v, m)
-read (Pending s p) = Stream.read (p s)
+read :: (Monad m) => (Stream m e r v) -> m (Chunk e r v, Stream m e r v)
+read (End r)     = return (Left (Right r), End r)
+read (Error e)   = return (Left (Left e), Error e)
+read (Data v m)  = return (Right v, m)
+read (Pending s) = s >>= \s' -> Stream.read s'
 
-runStream (End)         = []
-runStream (Error e)     = [Left e]
-runStream (Data v s )   = Right v : runStream s
-runStream (Pending s p) = runStream (p s)
+runStream :: (Monad m) => (Stream m e r v) -> m [Chunk e r v]
+runStream (End r)     = return [Left (Right r)]
+runStream (Error e)   = return [Left (Left e)]
+runStream (Data v s)  = runStream s >>= \s' -> return (Right v : s')
+runStream (Pending s) = s >>= \s' -> runStream s'
 
-instance (Show v, Show e) => Show (Stream s e v) where
-  show (End)         = ";"
-  show (Error e)     = "!" ++ show e
-  show (Data v s )   = show v ++ show s
-  show (Pending _ _) = "..."
+instance (Show v, Show e, Show r) => Show (Stream m e r v) where
+  show (End r)     = "->" ++ show r
+  show (Error e)   = "!>" ++ show e
+  show (Data v s ) = show v ++ show s
+  show (Pending _) = "..."
 
-instance Functor (Stream s e) where
-  fmap f (End)         = End
-  fmap f (Error e)     = Error e
-  fmap f (Data v s)    = Data (f v) (fmap f s)
-  fmap f (Pending s p) = Pending s (\s -> fmap f (p s))
-
-instance Monad (Stream s e) where
-  return v = Data v End
-  (>>=) (End)         _ = End
-  (>>=) (Error e)     _ = Error e
-  (>>=) (Data v s)    f = mplus (f v) (s >>= f)
-  (>>=) (Pending s p) f = Pending s (\s -> (p s) >>= f)
-
-instance MonadPlus (Stream s e) where
-  mzero = End
-  mplus (End)         s  = s
-  mplus (Error e)     _  = Error e
-  mplus (Data v s)    s' = Data v (mplus s s')
-  mplus (Pending s p) s' = Pending s (\s -> mplus (p s) s')
+instance (Monad m) => Functor (Stream m e r) where
+  fmap f (End r)     = End r
+  fmap f (Error e)   = Error e
+  fmap f (Data v s)  = Data (f v) (fmap f s)
+  fmap f (Pending s) = Pending (s >>= \s' -> return (fmap f s'))
--- StreamReader.hs
+++ StreamReader.hs
@@ -1,26 +1,46 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module StreamReader where
 
 import Stream
 import System.Random
-import Data.Functor.Identity
+import Control.Applicative
 import Control.Monad
 import Control.Monad.State
 
-produce :: (s -> Chunk e (s, v)) -> s -> Stream s e v
-produce p s = case p s of
-  (Left Nothing) -> End
-  (Left (Just e)) -> Error e
-  (Right (s, v)) -> Data v (Pending s (produce p))
+producer :: (Functor m) => m (Chunk e r v) -> Chunk e r v -> Stream m e r v
+producer p (Left (Left e))  = Error e
+producer p (Left (Right r)) = End r
+producer p (Right v)        = Data v (produce p)
 
-stream2 :: (Int, StdGen) -> (Chunk e ((Int, StdGen), Int))
-stream2 (3, gen) = (Left Nothing)
-stream2 (n, gen) = let (i, gen2) = next gen in Right ((n+1, gen2), i)
+produce :: (Functor m) => m (Chunk e r v) -> Stream m e r v
+produce p = Pending (fmap (producer p) p)
 
-type StreamReaderT s e v = StateT (Stream s e v)
+newtype StreamReaderT e r v m s = StreamReaderT { getStateT :: StateT (Stream m e r v) m s }
+  deriving (Functor, Applicative, Monad, MonadIO)
 
-read :: (Monad m) => StreamReaderT s e v m (Chunk e v)
-read = get >>= (\(out, st) -> put st >> return out) . Stream.read
+runReaderT (StreamReaderT s) = evalStateT s
 
+read :: (Monad m) => StreamReaderT e r v m (Chunk e r v)
+read = StreamReaderT $ StateT Stream.read
+
+{- example -}
+
+streamState :: State StdGen (Chunk e String Int)
+streamState = do
+  gen <- get
+  let (i, gen2) = next gen
+  put gen2
+  return (Right i)
+
+streamIO :: IO (Chunk e String Int)
+streamIO = do
+  gen <- getStdGen
+  let (i, gen2) = next gen
+  setStdGen gen2
+  return (Right i)
+
+readTest :: (Monad m) => StreamReaderT e r v m [Chunk e r v]
 readTest = do
   a <- StreamReader.read
   b <- StreamReader.read
@@ -29,5 +49,4 @@ readTest = do
   return [a, b, c, d]
 
 readTestRun = do
-  gen <- newStdGen
-  evalStateT readTest (fmap (2*) (produce stream2 (0, gen)))
+  runReaderT readTest (produce streamIO)
